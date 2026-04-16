1b42e63ae7077f83ed1e2a8d047693e42661b32a
hacker1234-crypto <chingyai33@gmail.com>
2026-04-16 13:31:52 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const mineflayer = require('mineflayer');
+
+// This keeps the bot alive on Render
+http.createServer((req, res) => {
+  res.write('AFK Bot is running!');
+  res.end();
+}).listen(process.env.PORT || 8080);
+
+const bot = mineflayer.createBot({
+  host: 'LordSMP.play.hosting', 
+  port: 25565,
+  username: 'Regnarok_Bot' 
+});
+
+bot.on('login', () => console.log('Bot is in the server!'));
+bot.on('kicked', (reason) => console.log('Kicked for: ' + reason));
+bot.on('error', (err) => console.log(err));
+
+// Auto-reconnect if it gets disconnected
+bot.on('end', () => {
+    console.log('Disconnected. Retrying in 10s...');
+    setTimeout(() => { process.exit(); }, 10000); // Forces Render to restart it
+});
+
